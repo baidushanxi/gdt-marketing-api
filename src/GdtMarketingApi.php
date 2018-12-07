3fe0fc476e38b78d3026c0d91c6e77e6dffae144
wangzhongjie <baidushanxi@vip.qq.com>
2018-12-07 11:43:12 +0800

    init

---
+++ src/GdtMarketingApi.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzhongjie  Email: baidushanxi@vip.qq.com
+ * Date: 2018/12/7
+ * Time: 上午11:39
+ */
+
+namespace Baidushanxi;
+
+
+class GdtMarketingApi
+{
+
+}
